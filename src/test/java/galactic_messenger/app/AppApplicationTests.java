ca074fc4c6870aa3bc42cd58ad0ce9d232b642d1
thibault-kine <thibault.kine@laplateforme.io>
2023-10-16 14:25:12 +0200

    Initial commit

---
+++ src/test/java/galactic_messenger/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package galactic_messenger.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
