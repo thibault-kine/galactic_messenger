984724342bc4e8537e2c8616d7ea009faed5b33f
Abraham-Saindou <abraham.saindou@laplateforme.io>
2023-10-19 03:13:16 +0200

    server works on ipv4

--- src/main/java/galactic_messenger/app/GalacticMessengerApplication.java
+++ src/main/java/galactic_messenger/app/GalacticMessengerApplication.java
@@ -1,9 +1,7 @@
 package galactic_messenger.app;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.net.*;
 import java.util.Collections;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -11,28 +9,44 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class GalacticMessengerApplication {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws SocketException {
         int port = 8080;
         port = Integer.parseInt(args[0]);
+        String ip = get_ip();
         
         // Lancement de l'app Spring Boot
         SpringApplication app = new SpringApplication(GalacticMessengerApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("server.address", ip));
         app.setDefaultProperties(Collections.singletonMap("server.port", port));
         ConfigurableApplicationContext context = app.run(args);
 
-        // Récupère l'IP et le port
-        try {
-            String ip = InetAddress.getLocalHost().getHostAddress();
-            System.out.printf("\nServer available at %s:%d\n", ip, port);
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
+        // Récupère l'ip et le port
+        // String ip = InetAddress.getLocalHost().getHostAddress(); This method isn't reliable, frequently returns localhost
+
+        System.out.printf("\nServer available at %s:%d\n", ip, port);
 
         // Démarre la console du serveur
         ArgumentHandler argumentHandler = new ArgumentHandler();
         argumentHandler.run();
 
         context.close();
+    }
+    public static String get_ip() throws SocketException {
+        String ip = null;
+
+        for (NetworkInterface ni :
+                Collections.list(NetworkInterface.getNetworkInterfaces())) {
+            for (InetAddress address : Collections.list(ni.getInetAddresses())) {
+                if (address instanceof Inet4Address && address.isLoopbackAddress()) { // Discard any loopback ip
+                    continue;
+                }
+                if (address instanceof Inet4Address){ // Takes ipv4 address and remove useless char
+                    String temp = address.toString();
+                    ip = temp.replace("/","");
+                    System.out.println(ip);
+                }
+            }
+        }
+        return ip;
     }
 }
